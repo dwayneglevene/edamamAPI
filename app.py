85cf1a32db5cc665b158b78f2426bf942771b9e0
dwayneglevene <dwayneglevene@yahoo.com>
2020-10-18 21:20:13 +0000

    api first commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -14,3 +14,9 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+@app.route("/yourgif",methods=["GET","POST"])
+def yourgif():
+    query = request.form["gifchoice"]
+    print(query)
+    image= getImageUrlFrom(query)
+    return render_template("yourgif.html",time=datetime.now(),image=image)
--- model.py
+++ model.py
@@ -1,2 +1,7 @@
+import requests
 def getImageUrlFrom(query):
-    pass
+    endpoint = f"https://api.edamam.com/search?q={query}&app_id=68dab55a&app_key=ff337fe378ee71408ee88720420075d4&from=0&to=3"
+    response = requests.get(endpoint).json()
+    # response2 = requests.get(endpoint).json()
+    return (response["hits"])
+    # return (response["hits"][1]['recipe'])
