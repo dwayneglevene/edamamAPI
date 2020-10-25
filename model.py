ec79c3500716b597da91911f589250e444e0cb99
dwayneglevene <dwayneglevene@yahoo.com>
2020-10-25 17:18:43 +0000

    trying to deploy api to heroku

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import requests
 def getImageUrlFrom(query):
-    endpoint = f"https://api.edamam.com/search?q={query}&app_id=68dab55a&app_key=ff337fe378ee71408ee88720420075d4&from=0&to=3"
+    endpoint = f"https://api.edamam.com/search?q={query}&app_id=68dab55a&app_key=ff337fe378ee71408ee88720420075d4&from=0&to=4"
     response = requests.get(endpoint).json()
     # response2 = requests.get(endpoint).json()
     return (response["hits"])
